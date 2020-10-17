45f79e2639720dfdc091f381cfffbe040b34e038
Leonardo <leomaucano111@gmial.com>
2020-10-17 10:45:32 -0300

    index de catgorias usuarios

---
+++ categorias/index.php
@@ -0,0 +1,23 @@
+<?php require_once '../libs/header.php' ?>
+
+<main class="contenedor inicio">
+    <h2 class="centrar__texto">Control de Categorias</h2>
+    <table class="tabla">
+        <thead>
+            <th>Nombre</th>
+            <th>Acciones</th>
+        </thead>
+        <tbody>
+            <tr>
+                <td>lorem1</td>
+                <td><button class="btn btn__danger acciones">lorem</button><button class="btn btn__ok acciones">lorem</button></td>
+            </tr>
+            <tr>
+                <td>lorem2</td>
+                <td><button class="btn btn__ok acciones">lorem</button> <button class="btn btn__danger acciones">lorem</button></td>
+            </tr>
+        </tbody>
+    </table>
+</main>
+
+<?php require_once '../libs/footer.php' ?>
---
+++ usuarios/index.php
@@ -0,0 +1,29 @@
+<?php require_once '../libs/header.php' ?>
+
+<main class="contenedor inicio">
+    <h2 class="centrar__texto">Control de Usuarios</h2>
+    <table class="tabla">
+        <thead>
+            <th>Nombre</th>
+            <th>DNI</th>
+            <th>Email</th>
+            <th>Acciones</th>
+        </thead>
+        <tbody>
+            <tr>
+                <td>lorem1</td>
+                <td>lorem1</td>
+                <td>lorem1</td>
+                <td><button class="btn btn__danger acciones">lorem</button></td>
+            </tr>
+            <tr>
+                <td>lorem2</td>
+                <td>lorem2</td>
+                <td>lorem2</td>
+                <td><button class="btn btn__ok acciones">lorem</button></td>
+            </tr>
+        </tbody>
+    </table>
+</main>
+
+<?php require_once '../libs/footer.php' ?>
