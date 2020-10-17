007f8b5931b7bb75cb8c5f98f2153391a7246a63
Leonardo <leomaucano111@gmial.com>
2020-10-17 00:17:51 -0300

    detalles de libro listo

---
+++ views/libros/detalleLibro.php
@@ -0,0 +1,24 @@
+<?php require_once '../../libs/header.php'; ?>
+
+<main class="contenedor inicio">
+    <div class="detalle__libros">
+        <div class="img__libro--detalle">
+            <img src="http://localhost/biblioteca2/assets/img/angeles-caidos-susan-ee-oceano-gran-travesia-juvenil-romantico-jr-cubierta-hd-calidad-portada.jpg" alt="">
+        </div>
+        <div class="detalles">
+            <h2>titulos van aqui</h2>
+            <p>Por <span class="autor">autor</span></p>
+            <div class="categoria">
+                categoria
+            </div>
+            <p class="format">lorem ipsim</p>
+            <h4>Sinopsis</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At eveniet officiis eligendi, sapiente ut laudantium, architecto itaque sint deserunt eum aliquam est quas asperiores corrupti tempore, cupiditate repudiandae a. Numquam.</p>
+            <div class="positon__up">
+                <button class="btn btn__celeste">Agregar al Carrito</button>
+            </div>
+        </div>
+    </div>
+</main>
+
+<?php require_once '../../libs/footer.php'; ?>
