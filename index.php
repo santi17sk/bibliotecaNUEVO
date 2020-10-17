094eb63e1ee0df6d96abc26a4e597e399d20c8cd
Leonardo <leomaucano111@gmial.com>
2020-10-16 22:16:25 -0300

    estilos de index listos

--- index.php
+++ index.php
@@ -1,6 +1,75 @@
 <?php require_once './libs/header.php'; ?>
-    <main class="contenedor inicio">
-        
-    </main>
+<main class="contenedor inicio">
+    <header class="libro__header">
+        <h2>Libros Populares</h2>
+        <input type="text" name="buscadorDeLibros" id="buscadorDeLibros" class="campo__input" placeholder="Buscar">
+    </header>
+    <div class="libros__contenedor">
+        <div class="libro">
+            <div class="libro__imagen">
+                <img src="http://localhost/biblioteca2/assets/img/51b7XbfMIIL.jpg" alt="Imagen del libro">
+            </div>
+            <div class="titulo__libro">
+                <h3>Los angeles caidos</h3>
+            </div>
+        </div>
+        <div class="libro">
+            <div class="libro__imagen">
+                <img src="http://localhost/biblioteca2/assets/img/angeles-caidos-susan-ee-oceano-gran-travesia-juvenil-romantico-jr-cubierta-hd-calidad-portada.jpg" alt="Imagen del libro">
+            </div>
+            <div class="titulo__libro">
+                <h3>Los angeles caidos</h3>
+            </div>
+        </div>
+        <div class="libro">
+            <div class="libro__imagen">
+                <img src="http://localhost/biblioteca2/assets/img/Captura_de_pantalla_2019-01-19_a_las_21.22.29-original.jpg" alt="Imagen del libro">
+            </div>
+            <div class="titulo__libro">
+                <h3>Los angeles caidos</h3>
+            </div>
+        </div>
+        <div class="libro">
+            <div class="libro__imagen">
+                <img src="http://localhost/biblioteca2/assets/img/descargar.jpg" alt="Imagen del libro">
+            </div>
+            <div class="titulo__libro">
+                <h3>Los angeles caidos</h3>
+            </div>
+        </div>
+        <div class="libro">
+            <div class="libro__imagen">
+                <img src="http://localhost/biblioteca2/assets/img/angeles-caidos-susan-ee-oceano-gran-travesia-juvenil-romantico-jr-cubierta-hd-calidad-portada.jpg" alt="Imagen del libro">
+            </div>
+            <div class="titulo__libro">
+                <h3>Los angeles caidos</h3>
+            </div>
+        </div>
+        <div class="libro">
+            <div class="libro__imagen">
+                <img src="http://localhost/biblioteca2/assets/img/angeles-caidos-susan-ee-oceano-gran-travesia-juvenil-romantico-jr-cubierta-hd-calidad-portada.jpg" alt="Imagen del libro">
+            </div>
+            <div class="titulo__libro">
+                <h3>Los angeles caidos</h3>
+            </div>
+        </div>
+        <div class="libro">
+            <div class="libro__imagen">
+                <img src="http://localhost/biblioteca2/assets/img/angeles-caidos-susan-ee-oceano-gran-travesia-juvenil-romantico-jr-cubierta-hd-calidad-portada.jpg" alt="Imagen del libro">
+            </div>
+            <div class="titulo__libro">
+                <h3>Los angeles caidos</h3>
+            </div>
+        </div>
+        <div class="libro">
+            <div class="libro__imagen">
+                <img src="http://localhost/biblioteca2/assets/img/angeles-caidos-susan-ee-oceano-gran-travesia-juvenil-romantico-jr-cubierta-hd-calidad-portada.jpg" alt="Imagen del libro">
+            </div>
+            <div class="titulo__libro">
+                <h3>Los angeles caidos</h3>
+            </div>
+        </div>
+    </div>
+</main>
 
 <?php require_once './libs/footer.php'; ?>
