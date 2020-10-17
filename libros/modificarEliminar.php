3dbe623615e0c3e7d24e34a3305e334a73d1bf73
Leonardo <leomaucano111@gmial.com>
2020-10-17 12:30:24 -0300

    cambios en el titulo

--- libros/modificarEliminar.php
+++ libros/modificarEliminar.php
@@ -1,6 +1,7 @@
 <?php require_once '../libs/header.php'; ?>
 
 <main class="contenedor inicio">
+    <h2 class="centrar__texto">Actualizar Libro</h2>
     <div class="detalle__libros">
         <div class="img__libro--detalle">
             <img src="http://localhost/biblioteca2/assets/img/angeles-caidos-susan-ee-oceano-gran-travesia-juvenil-romantico-jr-cubierta-hd-calidad-portada.jpg" alt="">
