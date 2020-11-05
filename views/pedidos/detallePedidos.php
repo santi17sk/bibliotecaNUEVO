5e345b8e329cb380c560a9bf28d2877728016bd9
Santi <santitito14@gmail.com>
2020-11-05 16:17:31 -0300

    cano gato

--- views/pedidos/detallePedidos.php
+++ views/pedidos/detallePedidos.php
@@ -24,38 +24,8 @@ require_once '../../libs/header.php';
 <main class="contenedor inicio">
     <h2 class="centrar__texto">Sus Reservas</h2>
     <div class="contenedor__pedidos">
-        <div class="pedido__res columnas__girs--6-4">
-            <div>hola</div>
-            <div>
-                <img loading="lazy" src="/biblioteca2/libros/img/51+NUIgEc9L._AC_UL600_SR462,600_.jpg" alt="" class="img__pedido" st>
-            </div>
-        </div>
-        <div class="pedido__res columnas__girs--6-4">
-            <div>hola</div>
-            <div>
-                <img loading="lazy" src="/biblioteca2/libros/img/IMG-20200911-WA0016.jpg" alt="" class="img__pedido">
-            </div>
-        </div>
-        <div class="pedido__res columnas__girs--6-4">
-            <div>hola</div>
-            <div>
-                <img loading="lazy" src="/biblioteca2/libros/img/IMG-20200911-WA0016.jpg" alt="" class="img__pedido">
-            </div>
-        </div>
-        <div class="pedido__res columnas__girs--6-4">
-            <div>hola</div>
-            <div>
-                <img loading="lazy" src="/biblioteca2/libros/img/IMG-20200911-WA0016.jpg" alt="" class="img__pedido">
-            </div>
-        </div>
-        <div class="pedido__res columnas__girs--6-4">
-            <div>hola</div>
-            <div>
-                <img loading="lazy" src="/biblioteca2/libros/img/IMG-20200911-WA0016.jpg" alt="" class="img__pedido">
-            </div>
-        </div>
+
     </div>
-    <!-- </div> -->
 </main>
 <?php
 require_once '../../libs/footer.php';
