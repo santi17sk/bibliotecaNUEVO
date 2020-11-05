92c41e12af773bc0bf9080ccc44f3170bd375130
Leonardo <leomaucano111@gmial.com>
2020-11-05 18:43:51 -0300

    validar el usuario este registrado para reservar un libro

--- js/ajax/usuarios/agregarCarrito.js
+++ js/ajax/usuarios/agregarCarrito.js
@@ -7,28 +7,34 @@
     const agregarLibro = (e) => {
         const cantidad = document.getElementById('numberDePedidos');
         const item = document.getElementById('itemPedidos');
-        console.log(cantidad, item)
+        // console.log(cantidad, item)
         const idLibro = e.target.getAttribute('data-id');
-        const xmlhttp = new XMLHttpRequest();
-        xmlhttp.open('POST', '/biblioteca2/usuarios/ajax/agregarCarrito.php');
-        xmlhttp.onreadystatechange = function () {
-            if (xmlhttp.readyState === 4) {
-                if (this.response !== '') {
-                    let data = JSON.parse(this.response)
-                    if (data.error != '') {
-                        alert(data.error)
-                    } else {
-                        if (data.cantidad === 1) {
-                            item.style.display = "block";
-                            cantidad.innerHTML = data.cantidad
+        if (idLibro == null) {
+            alert('Nesesita registrarse')
+        } else {
+            const xmlhttp = new XMLHttpRequest();
+            xmlhttp.open('POST', '/biblioteca2/usuarios/ajax/agregarCarrito.php');
+            xmlhttp.onreadystatechange = function () {
+                if (xmlhttp.readyState === 4) {
+                    if (this.response !== '') {
+                        let data = JSON.parse(this.response)
+                        if (data.error != '') {
+                            alert(data.error)
                         } else {
-                            cantidad.innerHTML = data.cantidad
+                            if (data.cantidad === 1) {
+                                item.style.display = "block";
+                                cantidad.innerHTML = data.cantidad
+                            } else {
+                                cantidad.innerHTML = data.cantidad
+                            }
                         }
                     }
                 }
-            }
-        };
-        xmlhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-        xmlhttp.send(`idLibro=${idLibro}`);
+            };
+            xmlhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+            xmlhttp.send(`idLibro=${idLibro}`);
+        }
+        console.log(idLibro)
+
     }
 })();
--- views/libros/detalleLibro.php
+++ views/libros/detalleLibro.php
@@ -4,29 +4,32 @@ if ($_REQUEST['idLibro']) {
     $id = $_REQUEST['idLibro'];
     $sql = "SELECT l.*, i.nombre, i.path, c.nombre as categoiaN, f.nombre as formatoN FROM libros l INNER JOIN imagenes i INNER JOIN categorias c inner join  formatos f ON l.id_imagen = i.id_imagen and c.id_categoria=l.id_categoria and f.id_formato=l.id_formato where id_libro = $id";
 
-$libros = prepare_select($conexion, $sql);
-if ($libros->num_rows>0) {
-    $libro = $libros->fetch_assoc();
-}
-
+    $libros = prepare_select($conexion, $sql);
+    if ($libros->num_rows > 0) {
+        $libro = $libros->fetch_assoc();
+    }
 }
 ?>
 <main class="contenedor inicio">
     <div class="columnas__girs--2">
         <div class="img__libro--detalle">
-            <img src="/biblioteca2/libros/img/<?=$libro['nombre']?>" alt="">
+            <img src="/biblioteca2/libros/img/<?= $libro['nombre'] ?>" alt="">
         </div>
         <div class="detalles">
-            <h2><?=$libro['titulo']?></h2>
-            <p>Por <span class="autor"><?=$libro['autor']?></span></p>
+            <h2><?= $libro['titulo'] ?></h2>
+            <p>Por <span class="autor"><?= $libro['autor'] ?></span></p>
             <div class="categoria">
-                <?=$libro['categoiaN']?>
+                <?= $libro['categoiaN'] ?>
             </div>
-            <p class="format">Disponible formato <?=$libro['formatoN']?></p>
+            <p class="format">Disponible formato <?= $libro['formatoN'] ?></p>
             <h4>Sinopsis</h4>
-            <p><?=$libro['descripcion']?></p>
+            <p><?= $libro['descripcion'] ?></p>
             <div class="positon__up">
-                <button class="btn_l btn__celeste" id="boton" data-id="<?=$libro['id_libro']?>">Agregar al Carrito</button>
+                <?php if (!empty($_SESSION['User'])) : ?>
+                    <button class="btn_l btn__celeste" id="boton" data-id="<?= $libro['id_libro'] ?>">Agregar al Carrito</button>
+                <?php else : ?>
+                    <button class="btn_l btn__celeste" id="boton">Agregar al Carrito</button>
+                <?php endif ?>
             </div>
         </div>
     </div>
